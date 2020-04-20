17f47a37dcff59559ff1090e439cad34b77f071f
emmanuel akachukwu <emmanuelakachukwu1@gmail.com>
2020-04-20 08:15:07 +0100

    first commit

--- python-task3.py
+++ python-task3.py
@@ -4,24 +4,25 @@ while True:
     level = input("Choose your level (easy, medium or hard): ")
     if level == "easy":
         guess_limit = 6
-        secret_number = random.randint(1, 10)
+        max_number = 10
+        secret_number = random.randint(1, max_number)
         break
     elif level == "medium":
         guess_limit = 4
-        secret_number = random.randint(1, 20)
-        print(secret_number)
+        max_number = 20
+        secret_number = random.randint(1, max_number)
         break
     elif level == "hard":
         guess_limit = 3
-        secret_number = random.randint(1, 50)
-        print(secret_number)
+        max_number = 50
+        secret_number = random.randint(1, max_number)
         break
     else:
         continue
 
 guess_count = 1
 while guess_count <= guess_limit :
-    guess = int(input("Guess the secret number from 1 - 10: "))
+    guess = int(input(f"Guess the secret number from 1 - {max_number}: "))
     if guess == secret_number :
         print("You got it right")
         break
